6b12e2bacea9ecd8b78ecbf0ebb0130fc3330081
Lily Hoopes <lilyhoopes@gmail.com>
2026-02-24 13:37:46 -0800

    all walkign direction workisn

--- lily.js
+++ lily.js
@@ -47,14 +47,14 @@ class Lily {
         const sheet = ASSET_MANAGER.getAsset("./Sprites/LilySpriteSheet2_0.png");
 
         this.animations = {
-            idleDown:      new Animator(sheet, 0, 0,   270, 135, 4, 0.6),  // row 1: idle down
-            walkDown:  new Animator(sheet, 0, 135, 270, 135, 4, 0.15), // row 2: walk down
-            walkLeft:  new Animator(sheet, 0, 270, 270, 135, 4, 0.15), // row 3: walk left
-            walkRight: new Animator(sheet, 0, 405, 270, 135, 4, 0.15), // row 4: walk right
-            walkUp:    new Animator(sheet, 0, 540, 270, 135, 4, 0.15), // row 5: walk up
-            idleUp:    new Animator(sheet, 0, 675, 270, 135, 4, 0.6),  // row 6: idle up
-            idleLeft:  new Animator(sheet, 0, 810, 270, 135, 4, 0.6),  // row 7: idle left
-            idleRight: new Animator(sheet, 0, 945, 270, 135, 4, 0.6),  // row 8: idle right
+            idleDown:  new Animator(sheet, 0, 60,   803, 928, 2, 0.6),  // row 1: idle down
+            walkDown:  new Animator(sheet, 0, 928,  803, 744, 4, 0.15), // row 2: walk down
+            walkLeft:  new Animator(sheet, 0, 1760, 830, 744, 4, 0.15), // row 3: walk left
+            walkRight: new Animator(sheet, 0, 2525, 830, 744, 4, 0.15), // row 4: walk right
+            walkUp:    new Animator(sheet, 0, 3270, 830, 744, 4, 0.15), // row 5: walk up
+            idleUp:    new Animator(sheet, 0, 675,  270, 135, 2, 0.6),  // row 6: idle up
+            idleLeft:  new Animator(sheet, 0, 810,  270, 135, 2, 0.6),  // row 7: idle left
+            idleRight: new Animator(sheet, 0, 945,  270, 135, 2, 0.6),  // row 8: idle right
         };
         
         // Track current state
